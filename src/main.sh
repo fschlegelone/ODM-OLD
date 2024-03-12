ded2c3c342e32c20869f970d90495957ec599dcc
fschlegelone <fabian@schlegel.one>
2024-03-12 08:33:02 +0100

    add: example dotfiles

--- src/main.sh
+++ src/main.sh
@@ -8,14 +8,18 @@ source ./config.sh
 main() {
     gum style --border normal --margin "1" --padding "1 2" --align "center" --border-foreground "$HEX_HL2" "Welcome to $(gum style --foreground "$HEX_HL1" 'ODM')!" "The first Dotfile Manager, that doesn't suck."
     # Action Variables
+    INIT="ODM Initialization (First Time Setup)"
     STORAGE="Dotfile Storage"
     HOMEDIR="Home Directory"
     STATUS="Show Status"
     # Prompt User for Action
-    ACTION=$(gum choose --item.foreground "$HEX_FG1" --cursor.foreground "$HEX_HL1" --cursor " " "$STORAGE" "$HOMEDIR" "$STATUS")
+    ACTION=$(gum choose --item.foreground "$HEX_FG1" --cursor.foreground "$HEX_HL1" --cursor " " "$INIT" "$STORAGE" "$HOMEDIR" "$STATUS")
 
+    # Action: Init Script
+    if [ "$ACTION" == "$INIT" ]; then
+        bash ./init.sh
     # Action: Dotfile Storage
-    if [ "$ACTION" == "$STORAGE" ]; then
+    elif [ "$ACTION" == "$STORAGE" ]; then
         bash ./storage.sh
     # Action: Home Directory
     elif [ "$ACTION" == "$HOMEDIR" ]; then
